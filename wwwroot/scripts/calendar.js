408fb5863d213dc1c09db5866523c2cdddcb8b0a
Rikard Friberg <ri.frib@gmail.com>
2021-06-11 16:06:32 +0200

    Adjusted placement of dates in calender
    Co-authored-by: Emelie Broberg <EmelieBrobergs@users.noreply.github.com>

--- wwwroot/scripts/calendar.js
+++ wwwroot/scripts/calendar.js
@@ -1,12 +1,34 @@
-window.addEventListener('load', main);
+window.addEventListener('load', calendar);
 
-function main() {
-    var date = new Date();
-    var month = getMonthString(date);
-    var year = date.getUTCFullYear();
+var date = new Date();
+var month = getMonthString(date);
+var year = date.getUTCFullYear();
+var gridElement;
 
 
+
+function calendar() {
+    callAPI(year, month); 
+}
+
+//TODO: ordan med månader och år!!
+
+function nextMonth() {
+    month++;
+    reset();    
     callAPI(year, month);
+}
+
+function prevMonth() {
+    month--;
+    reset();
+    callAPI(year, month);
+}
+
+function reset() {
+    document
+      .querySelectorAll(".date")
+      .forEach((e) => e.parentNode.removeChild(e));
 }
 
 // NOTE: Responseformat from API
@@ -31,25 +53,30 @@ function main() {
 // };
 
 
-async function callAPI (year, month) {
-     
+async function callAPI (year, month) { 
     
     var response = await fetch('https://sholiday.faboul.se/dagar/v2.1/' + year + '/' + month, {
         method: 'GET'
     });
     var jsonData = await response.json();
-    // TODO: Add id="grid" to: <div id="grid" class="grid-container">
-    const gridElement = document.getElementById('grid');
+    let firstDay = true;
 
-    // TODO: Gör så dagarna hamnar på rätt veckodag
+    // TODO: Add id="grid" to: <div id="grid" class="grid-container">
+    gridElement = document.getElementById('grid');
+    //loop through all dates of the given month
     for (var item of jsonData.dagar) {
-        var div = document.createElement('div');
+        if (firstDay) {
+            createSpace(parseInt(item['dag i vecka']))
+            firstDay = false;
+        }
+        let div = document.createElement('div');
         div.classList.add('grid-item');
+        div.classList.add('date');
         var p = document.createElement('p');
-        // if (item.röddag === 'Ja')
-        // {
-        //     div.classList.add('redday');
-        // }
+        if (item['röd dag'] === 'Ja')
+        {
+            div.classList.add('redday');
+        }
         if (item.helgdag)
         {
             var holiday = document.createElement('p');
@@ -61,7 +88,21 @@ async function callAPI (year, month) {
         p.innerText = item.datum;
         div.append(p);
 
-        gridElement.append(div);        
+        gridElement.append(div); 
+
+        /**
+         * Calculate the startpoint for the first day of the month 
+         */
+        function createSpace(dayOfWeek) {
+            
+            for (let index = 0; index < (dayOfWeek - 1); index++) {
+                var div2 = document.createElement('div');
+                // div2.classList.add('grid-item');
+                div2.classList.add('grid-item-empty');
+                div2.classList.add('date');
+                gridElement.append(div2);    
+            }
+        }
     } 
 }
 
